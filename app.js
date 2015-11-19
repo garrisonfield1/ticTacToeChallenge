80056e9ab6bf3fa6d6e5ec71ae899e659d5b35ab
Sean Mcintyre <sean_mcintyre@hotmail.com>
2015-11-19 13:00:30 -0500

    added strategyRandom() fun for compMove

--- app.js
+++ app.js
@@ -48,18 +48,53 @@ function assignSquare(symbol, square){
 }
 
 
-function compMove(){
-    for(var prop in board){
-        console.log("there are " + prop in + board[prop]);
-
+function getLegalMoves(board){
+    var moves = 0;
+    for (var i=0; i<9; i++){
+        if ((board & (1<<(i*2+1))) == 0){
+            moves |= 1 << i;
+        }
     }
-};
+    return moves;
+}
+
+// go in a randomly selected blank space
+function strategyRandom() {
+    // gather all the blank spots in an array
+    var blanks = [];
+    for (var x=0; x<3; x++) {
+        for (var y=0; y<3; y++) {
+            if (val(x,y)=='') blanks.push([x,y]);
+        }
+    }
+    // return a random entry in the array of blanks
+    if (blanks.length>0) {
+        var r = Math.floor((Math.random()*blanks.length));
+        return blanks[r];
+    }
+    else return false;
+}
+
+function option(name) {
+    //return $("input[name='"+name+"']")[0].checked;
+}
+
+function compMove() {
+    var strategies = [];
+    if (option('random')) strategies.push(strategyRandom);
+    for (var i=0; i<strategies.length; i++) {
+        var turn = strategies[i]();
+        if (!turn) continue;
+        val(turn[0], turn[1], computer);
+        break;
+    }
+}
+
 
 // click a square and save to board array
 document.getElementById("board").addEventListener("click", function(e) {
     // identify clicked targetSquare
     if(e.target && e.target.nodeName == "DIV") {
-        var targetSquareId = e.target.id;
 
         // assign div data-index to var
         var square = e.target.dataset.index;
@@ -70,16 +105,13 @@ document.getElementById("board").addEventListener("click", function(e) {
 
             // ammend X or O to targetSquare UI
             e.srcElement.classList.add(userTurn);
-
-            changeTurn();
-
-            compMove();
-
-
         }
-
+        changeTurn();
+        compMove();
     }
 });
 
+
+//randomMove();
 
 
